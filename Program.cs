319019041c36f053cbd64cbdf4bac85fd8e1222d
ZhMike <zh-mike@mail.ru>
2022-10-11 15:04:58 +0500

    have done it

--- Program.cs
+++ Program.cs
@@ -1,17 +1,36 @@
-﻿// Конторльная кабота по первой четверти
-void CheckArrey(string[] arrey, int num)
+﻿// Контрольная работа по первой четверти
+
+// Считаем сколько элементов подходит под условие, что бы создать новый массив нужной длинны
+int CountArrey(string[] arrey, int num)
 {
+    int count = 0;
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        if(arrey[i].Length <= num) count++; 
+    }
+    return count;
+}
+
+// Зпролняем новый массив и выводим ответ
+void CheckArrey(string[] arrey, string[] newArrey, int num)
+{
+    int j = 0;
     for (int i = 0; i < arrey.Length; i++)
     {
         if(arrey[i].Length <= num)
-            Console.Write($"{arrey[i]} ");
+        {
+            newArrey[j] = arrey[i];
+            j++;
+        }      
     }
-
+    Console.WriteLine("[" + string.Join(", ", newArrey) + "]");
 }
 
 string [] arrey = {"hello", "2", "world", ":-)"};
 int num = 3; // Максимальная длинна выводимой строки
 Console.WriteLine("[" + string.Join(", ", arrey) + "]");
-CheckArrey(arrey, num);
+string[] newArrey = new string[CountArrey(arrey, num)];
+CheckArrey(arrey, newArrey, num);
+
 
 
